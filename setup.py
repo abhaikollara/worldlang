e330f65a5a4a4315d817224081863c113b8cc416
Abhai Kollara Dilip <abhai.dilip@gmail.com>
2018-10-13 18:15:48 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="Worldlang",
+    version="0.1",
+    description="A language for building and querying world models.",
+    author="Fariz Rahman",
+    author_email="farizrahman4u@gmail.com",
+    url="https://github.com/farizrahman4u/worldlang",
+    packages=find_packages(),
+)
